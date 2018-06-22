fb394d8e3912ec7c2887407ca576a44cac960975
FREECOMM\yujie.feng <yujie.feng@phicomm.com>
2018-06-22 13:55:15 +0800

    first commit

---
+++ src/main/java/com/phicomm/info/MyInfoApplication.java
@@ -0,0 +1,13 @@
+package com.phicomm.info;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyInfoApplication {
+
+    public static void main(String []args){
+        SpringApplication.run(MyInfoApplication.class);
+    }
+
+}
