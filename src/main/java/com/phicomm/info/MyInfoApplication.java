6d28b62e427fab2b44d29ceab54bf83ff50190e4
FREECOMM\yujie.feng <yujie.feng@phicomm.com>
2018-06-22 17:00:03 +0800

    war 包格式

--- src/main/java/com/phicomm/info/MyInfoApplication.java
+++ src/main/java/com/phicomm/info/MyInfoApplication.java
@@ -2,9 +2,16 @@ package com.phicomm.info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyInfoApplication {
+public class MyInfoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyInfoApplication.class);
+    }
 
     public static void main(String []args){
         SpringApplication.run(MyInfoApplication.class);
